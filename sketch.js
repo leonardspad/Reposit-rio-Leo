2d3a97f7e997a04c15060abeb95f5880e27055c4
Leonardo Spadacio <00001089542689SP@al.educacao.sp.gov.br>
2024-06-05 07:50:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  
+
+  stroke("blue");
+  fill("red");
+  
+    if (mouseIsPressed){
+    rect(mouseX, mouseY,20,35,);
+    }
+}
